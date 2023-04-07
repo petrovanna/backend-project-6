24c5c64a7eb19c720f047e991cf55d92b8410411
Anna Petrova <petrovanna91@mail.ru>
2023-04-07 22:19:52 +0200

    Add Task model

---
+++ server/models/Task.cjs
@@ -0,0 +1,58 @@
+// @ts-check
+
+const objectionUnique = require('objection-unique');
+const BaseModel = require('./BaseModel.cjs');
+const TaskStatus = require('./TaskStatus.cjs');
+const User = require('./User.cjs');
+
+const unique = objectionUnique({ fields: ['name'] });
+
+module.exports = class Task extends unique(BaseModel) {
+  static get tableName() {
+    return 'tasks';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'statusId', 'creatorId'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', minLength: 1 },
+        description: { type: 'string' },
+        statusId: { type: 'integer', minimum: 1 },
+        creatorId: { type: 'integer' },
+        executorId: { type: 'integer' },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      status: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: TaskStatus,
+        join: {
+          from: 'tasks.statusId',
+          to: 'task_statuses.id',
+        },
+      },
+      creator: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'tasks.creatorId',
+          to: 'users.id',
+        },
+      },
+      executor: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'tasks.executorId',
+          to: 'users.id',
+        },
+      },
+    };
+  }
+};
