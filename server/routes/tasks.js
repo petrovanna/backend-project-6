d29ebb7e7640829dccd91320ce169564ff055238
Anna Petrova <petrovanna91@mail.ru>
2023-04-07 22:42:45 +0200

    Add GET /tasks, GET /tasks/new, POST /tasks

---
+++ server/routes/tasks.js
@@ -0,0 +1,41 @@
+// @ts-check
+
+import i18next from 'i18next';
+
+export default (app) => {
+  app
+    .get('/tasks', { name: 'tasks', preValidation: app.authenticate }, async (req, reply) => {
+      const tasks = await app.objection.models.task.query();
+      reply.render('tasks/index', { tasks });
+      return reply;
+    })
+    .get('/tasks/new', { name: 'newTask', preValidation: app.authenticate }, (req, reply) => {
+      const task = new app.objection.models.task();
+      reply.render('tasks/new', { task });
+    })
+    .post('/tasks', { name: 'createTask', preValidation: app.authenticate }, async (req, reply) => {
+      const task = new app.objection.models.task();
+      task.$set(req.body.data);
+
+      try {
+        const validTask = await app.objection.models.task.fromJson(req.body.data);
+        await app.objection.models.task.query().insert(validTask);
+        req.flash('info', i18next.t('flash.tasks.create.success'));
+        reply.redirect(app.reverse('tasks'));
+      } catch (e) {
+        req.flash('error', i18next.t('flash.tasks.create.error'));
+        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', e);
+        reply.render('tasks/new', { task, errors: e });
+      }
+
+      return reply;
+    });
+};
+
+/* GET /tasks - страница со списком всех задач +
+GET /tasks/new - страница создания задачи +
+GET /tasks/:id - страница просмотра задачи
+GET /tasks/:id/edit - страница редактирования задачи
+POST /tasks - создание новой задачи +
+PATCH /tasks/:id - обновление задачи
+DELETE /tasks/:id - удаление задачи */
