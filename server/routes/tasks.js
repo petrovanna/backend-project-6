2afe2e2c0b8ca0173673589aca394a2d1537927e
Anna Petrova <petrovanna91@mail.ru>
2023-04-15 01:42:47 +0200

    Add routes

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -5,36 +5,101 @@ import i18next from 'i18next';
 export default (app) => {
   app
     .get('/tasks', { name: 'tasks', preValidation: app.authenticate }, async (req, reply) => {
-      const tasks = await app.objection.models.task.query();
-      reply.render('tasks/index', { tasks });
+      const users = await app.objection.models.user.query();
+      const statuses = await app.objection.models.taskStatus.query();
+      const tasks = await app.objection.models.task.query().withGraphJoined('[creator, executor, status]');
+
+      reply.render('tasks/index', { tasks, users, statuses });
       return reply;
     })
-    .get('/tasks/new', { name: 'newTask', preValidation: app.authenticate }, (req, reply) => {
+    .get('/tasks/new', { name: 'newTask', preValidation: app.authenticate }, async (req, reply) => {
       const task = new app.objection.models.task();
-      reply.render('tasks/new', { task });
+      const users = await app.objection.models.user.query();
+      const statuses = await app.objection.models.taskStatus.query();
+
+      reply.render('tasks/new', { task, statuses, users });
+      return reply;
     })
     .post('/tasks', { name: 'createTask', preValidation: app.authenticate }, async (req, reply) => {
       const task = new app.objection.models.task();
+      const users = await app.objection.models.user.query();
+      const statuses = await app.objection.models.taskStatus.query();
+      const { id: creatorId } = req.user;
+      const {
+        name, description, statusId, executorId,
+      } = req.body.data;
+      const taskData = {
+        name,
+        description,
+        statusId: Number(statusId),
+        executorId: Number(executorId),
+        creatorId,
+      };
       task.$set(req.body.data);
 
       try {
-        const validTask = await app.objection.models.task.fromJson(req.body.data);
+        const validTask = await app.objection.models.task.fromJson(taskData);
         await app.objection.models.task.query().insert(validTask);
         req.flash('info', i18next.t('flash.tasks.create.success'));
         reply.redirect(app.reverse('tasks'));
       } catch ({ data }) {
         req.flash('error', i18next.t('flash.tasks.create.error'));
-        reply.render('tasks/new', { task, errors: data });
+        reply.render('tasks/new', {
+          task, users, statuses, errors: data,
+        });
       }
 
       return reply;
+    })
+    .get('/tasks/:id/edit', { name: 'editTask', preValidation: app.authenticate }, async (req, reply) => {
+      const task = await app.objection.models.task.query().findById(req.params.id);
+      const statuses = await app.objection.models.taskStatus.query();
+      const users = await app.objection.models.user.query();
+      reply.render('/tasks/edit', { task, statuses, users });
+      return reply;
+    })
+    .patch('/tasks/:id', async (req, reply) => {
+      const { id } = req.params;
+      const task = await app.objection.models.task.query().findById(id);
+      const users = await app.objection.models.user.query();
+      const statuses = await app.objection.models.taskStatus.query();
+      const { id: creatorId } = req.user;
+      const {
+        name, description, statusId, executorId,
+      } = req.body.data;
+      const taskData = {
+        name,
+        description,
+        statusId: Number(statusId),
+        executorId: Number(executorId),
+        creatorId,
+      };
+      try {
+        await task.$query().update(taskData);
+        req.flash('info', i18next.t('flash.tasks.update.success'));
+        reply.redirect('/tasks');
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.tasks.update.error'));
+        reply.redirect((`/tasks/${id}/edit`), task, users, statuses, { errors: data });
+      }
+    })
+    .delete('/tasks/:id', { name: 'deleteTask', preValidation: app.authenticate }, async (req, reply) => {
+      const { id } = req.params;
+      try {
+        await app.objection.models.task.query().deleteById(id);
+        req.flash('info', i18next.t('flash.tasks.delete.success'));
+      } catch (err) {
+        req.flash('error', i18next.t('flash.tasks.delete.error'));
+        reply.redirect(app.reverse('tasks'));
+      }
+      reply.redirect(app.reverse('tasks'));
+      return reply;
+    })
+    .get('/tasks/:id', { name: 'taskPage', preValidation: app.authenticate }, async (req, reply) => {
+      const taskId = Number(req.params.id);
+      const task = await app.objection.models.task.query().withGraphJoined('[creator, executor, status]').findById(taskId);
+
+      reply.render('tasks/task', { task });
+      return reply;
     });
 };
-
-/* GET /tasks - страница со списком всех задач +
-GET /tasks/new - страница создания задачи +
-GET /tasks/:id - страница просмотра задачи
-GET /tasks/:id/edit - страница редактирования задачи
-POST /tasks - создание новой задачи +
-PATCH /tasks/:id - обновление задачи
-DELETE /tasks/:id - удаление задачи */
