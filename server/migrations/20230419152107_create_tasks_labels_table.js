e344a80840772c81afa26eaabf2d63c985783313
Anna Petrova <petrovanna91@mail.ru>
2023-04-19 23:55:56 +0200

    Add tasks_labels table

---
+++ server/migrations/20230419152107_create_tasks_labels_table.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+export const up = (knex) => (
+    knex.schema.createTable('tasks_labels', (table) => {
+      table.integer('task_id').references('id').inTable('tasks').onDelete('CASCADE');
+      table.integer('label_id').references('id').inTable('labels').onDelete('CASCADE');
+    })
+  );
+  
+  export const down = (knex) => knex.schema.dropTable('tasks_labels');
