47fcef983cc58d63f3134e8084c8760cc817de61
Anna Petrova <petrovanna91@mail.ru>
2023-05-04 15:39:18 +0200

    Update rollbar token

--- server/plugin.js
+++ server/plugin.js
@@ -82,7 +82,7 @@ const addHooks = (app) => {
 
 const setErrorHandler = (app) => {
   const rollbar = new Rollbar({
-    accessToken: 'c3bba38883cd4b8fae03cbdd1e22d973',
+    accessToken: process.env.ROLLBAR_KEY,
     captureUncaught: true,
     captureUnhandledRejections: true,
   });
