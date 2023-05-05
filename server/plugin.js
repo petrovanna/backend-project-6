56ef890cd3717d1621fe5c129a54e00de56d5b64
Anna Petrova <petrovanna91@mail.ru>
2023-05-05 14:41:28 +0200

    Fix rollbar

--- server/plugin.js
+++ server/plugin.js
@@ -82,6 +82,7 @@ const addHooks = (app) => {
 
 const setErrorHandler = (app) => {
   const rollbar = new Rollbar({
+    enabled: process.env.NODE_ENV === 'production',
     accessToken: process.env.ROLLBAR_KEY,
     captureUncaught: true,
     captureUnhandledRejections: true,
