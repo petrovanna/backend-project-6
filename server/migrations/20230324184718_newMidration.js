48a2976b4d581ad799f95b7a0dea1cfba2cf0c34
Anna Petrova <petrovanna91@mail.ru>
2023-05-10 01:31:18 +0200

    Fix lint

--- server/migrations/20230324184718_newMidration.js
+++ server/migrations/20230324184718_newMidration.js
@@ -2,15 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-  export const up = (knex) => knex.schema.table('users', (table) => {
-    table.string('first_name').notNullable().defaultTo('');
-    table.string('last_name').notNullable().defaultTo('');
-  });
-  
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Promise<void> }
-   */
-  export const down = (knex) => knex.schema.table('users', (table) => {
-    table.dropColumns('first_name', 'last_name');
-  });
+export const up = (knex) => knex.schema.table('users', (table) => {
+  table.string('first_name').notNullable().defaultTo('');
+  table.string('last_name').notNullable().defaultTo('');
+});
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = (knex) => knex.schema.table('users', (table) => {
+  table.dropColumns('first_name', 'last_name');
+});
